59a5560a3e363e78355cd2752b8e05fd03ee1720
neil-coutinho <njcoutinho@gmail.com>
2021-07-27 09:05:46 +0530

    Using Next Link tag for routing

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+
 export default function Header() {
-  return <header>HEADER</header>;
+  return (
+    <header>
+      <div className="bar">
+        <Link href="/">Logo</Link>
+        <nav>Nav</nav>
+      </div>
+      <div className="sub-bar">Search</div>
+    </header>
+  );
 }
