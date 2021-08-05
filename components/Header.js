a7bdda350594bdd5e9a62e51a295a8a65bfbcc1c
neil-coutinho <njcoutinho@gmail.com>
2021-08-05 09:02:07 +0530

    Styled component basics

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,22 @@
+import styled from 'styled-components';
 import Link from 'next/link';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  color: green;
+  a {
+    color: red;
+  }
+`;
 
 export default function Header() {
   return (
     <header>
       <div className="bar">
-        <Link href="/">Logo</Link>
+        <Logo>
+          <Link href="/">Logo</Link>
+        </Logo>
+
         <Nav />
       </div>
       <div className="sub-bar">Search</div>
