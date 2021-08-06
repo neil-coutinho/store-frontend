81a88df6258916a7376caca4723292ac0ad15194
neil-coutinho <njcoutinho@gmail.com>
2021-08-06 08:59:14 +0530

    Header styles via styled components

--- components/Header.js
+++ components/Header.js
@@ -2,24 +2,46 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import Nav from './Nav';
 
-const Logo = styled.h1`
-  color: green;
+const LogoStyles = styled.h1`
+  background: red;
+  transform: skew(-7deg);
+  display: inline-block;
   a {
-    color: red;
+    color: #fff;
+    text-transform: uppercase;
+    padding: 1rem;
+    font-weight: bold;
+    padding: 0.5rem;
+    text-decoration: none;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    border-bottom: 1px solid var(--black, black);
+    grid-template-columns: 1fr auto;
   }
 `;
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Logo>
+        <LogoStyles>
           <Link href="/">Logo</Link>
-        </Logo>
-
+        </LogoStyles>
+      </div>
+      <div className="sub-bar">
         <Nav />
       </div>
-      <div className="sub-bar">Search</div>
-    </header>
+    </HeaderStyles>
   );
 }
