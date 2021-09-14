4d49b461023de3b1e22903669075de0a5a979723
Neil Coutinho <njcoutinho@gmail.com>
2021-09-14 08:49:07 +0530

    Setting up Apollo client

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,24 @@
-import Page from '../components/Page';
 // import 'nprogress/nprogress.css';
 import '../components/styles/nprogress.css';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
+import Page from '../components/Page';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 // Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
+  console.log({ apollo });
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+export default withData(MyApp);
