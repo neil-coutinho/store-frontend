c0237c8843f4d73ac4fedb2304aad369fbf56ccb
Neil Coutinho <njcoutinho@gmail.com>
2021-09-22 08:59:26 +0530

    add: Product ItemStyles

--- components/Product.js
+++ components/Product.js
@@ -1,7 +1,16 @@
 import PropTypes from 'prop-types';
+import ItemStyles from './styles/ItemStyles';
 
 export default function Product({ product }) {
-  return <div>{product.name}</div>;
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+      <p>{product.name}</p>
+    </ItemStyles>
+  );
 }
 
 Product.propTypes = {
