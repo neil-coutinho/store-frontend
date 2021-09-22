3d996a181a0f5f0f3b45ceaced9173774b5068b4
Neil Coutinho <njcoutinho@gmail.com>
2021-09-22 08:43:45 +0530

    add: Product component + grid styling

---
+++ components/Product.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export default function Product({ product }) {
+  return <div>{product.name}</div>;
+}
+
+Product.propTypes = {
+  product: PropTypes.any,
+};
--- components/Products.js
+++ components/Products.js
@@ -1,5 +1,7 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Product from './Product';
 
 const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
@@ -19,6 +21,12 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { data, errors, loading } = useQuery(ALL_PRODUCTS_QUERY);
   console.log(data, errors, loading);
@@ -32,11 +40,11 @@ export default function Products() {
   }
 
   return (
-    <div>
-      {data.allProducts.map((product) => {
-        console.log({ product });
-        return <div key={product.id}>{product.name}</div>;
-      })}
-    </div>
+    <ProductStyles>
+      {data.allProducts.map((product) => (
+        // console.log({ product });
+        <Product key={product.id} product={product} />
+      ))}
+    </ProductStyles>
   );
 }
