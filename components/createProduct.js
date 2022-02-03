0a0fe2350a9bb69120a6239cb4fabe75301f5ab5
Neil Coutinho <njcoutinho@gmail.com>
2022-02-03 13:57:12 +0530

    basic input form field with useState hook

--- components/createProduct.js
+++ components/createProduct.js
@@ -1,47 +1,25 @@
-// import { useState } from 'react';
-import useForm from '../lib/useForm';
-import FormStyles from './styles/Form';
+import { useState } from 'react';
 
 export default function CreateProduct() {
-  // const [name, setName] = useState('Neil');
-
-  const [inputs, handleChange, resetForm, clearForm] = useForm({
-    name: 'Neil Coutinho',
-    price: 10,
-  });
+  const [name, setName] = useState('Neil');
 
   return (
     <div>
-      <FormStyles>
-        <fieldset aria-busy="true">
-          <label htmlFor="name">Name</label>
+      <form>
+        <label htmlFor="name">
+          Name
           <input
-            id="name"
+            type="text"
             name="name"
-            placeholder="Product Name"
-            value={inputs.name}
-            onChange={handleChange}
+            id="name"
+            value={name}
+            onChange={(e) => {
+              const { value } = e.target;
+              setName(value);
+            }}
           />
-          <label htmlFor="price">Price</label>
-          <input
-            id="price"
-            name="price"
-            type="number"
-            placeholder="Product Price"
-            value={inputs.price}
-            onChange={handleChange}
-          />
-        </fieldset>
-
-        <button type="submit">Add Product</button>
-
-        {/* <button type="button" onClick={clearForm}>
-          Clear
-        </button>
-        <button type="button" onClick={resetForm}>
-          Reset Form
-        </button> */}
-      </FormStyles>
+        </label>
+      </form>
     </div>
   );
 }
