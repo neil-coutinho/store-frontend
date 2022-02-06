eb5871d1deae7f30457d5e9a4f4de439480d835d
Neil Coutinho <njcoutinho@gmail.com>
2022-02-06 07:54:30 +0530

    update: added Form styles to createProduct form

--- components/createProduct.js
+++ components/createProduct.js
@@ -1,26 +1,43 @@
 import useForm from '../lib/useForm';
+import Form from './styles/Form';
 
 export default function CreateProduct() {
   // const [name, setName] = useState('Neil');
 
-  const [formGroup, handleChange] = useForm({
+  const [formGroup, handleChange, resetForm, clearForm] = useForm({
     name: '',
+    price: '',
   }); // custom hook in place of useState
 
   return (
     <div>
-      <form>
-        <label htmlFor="name">
-          Name
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={formGroup.name}
-            onChange={handleChange}
-          />
-        </label>
-      </form>
+      <Form>
+        <fieldset>
+          <label htmlFor="name">
+            Name
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={formGroup.name}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label htmlFor="price">
+            Price
+            <input
+              type="text"
+              name="price"
+              id="price"
+              value={formGroup.price}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="submit">Add Product</button>
+        </fieldset>
+      </Form>
     </div>
   );
 }
