f63b4bd08a6b5aee478428078f6eca706c4106e9
Neil Coutinho <njcoutinho@gmail.com>
2022-02-06 08:22:04 +0530

    update: Create product form added image, description fields + onSubmit handler

--- components/createProduct.js
+++ components/createProduct.js
@@ -7,11 +7,17 @@ export default function CreateProduct() {
   const [formGroup, handleChange, resetForm, clearForm] = useForm({
     name: '',
     price: '',
+    description: '',
+    image: '',
   }); // custom hook in place of useState
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e, formGroup);
+  };
   return (
     <div>
-      <Form>
+      <Form onSubmit={onSubmit}>
         <fieldset>
           <label htmlFor="name">
             Name
@@ -27,11 +33,33 @@ export default function CreateProduct() {
           <label htmlFor="price">
             Price
             <input
-              type="text"
+              type="number"
               name="price"
               id="price"
               value={formGroup.price}
               onChange={handleChange}
+            />
+          </label>
+
+          <label htmlFor="description">
+            Description
+            <textarea
+              name="description"
+              id="description"
+              value={formGroup.description}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label htmlFor="image">
+            Image
+            <input
+              type="file"
+              name="image"
+              id="image"
+              value={formGroup.image}
+              onChange={handleChange}
+              required
             />
           </label>
 
