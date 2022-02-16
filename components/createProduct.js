80a9a0522e0f9dce09f150d8ca02e9f543efaeec
Neil Coutinho <njcoutinho@gmail.com>
2022-02-16 09:18:14 +0530

    fixed bug in useForm hook type === file instead of files added image to CREATE_PRODUCT_MUTATION

--- components/createProduct.js
+++ components/createProduct.js
@@ -6,103 +6,100 @@ import Form from './styles/Form';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
+    # Which variables are getting passed in? And What types are they
     $name: String!
-    $description: String
+    $description: String!
     $price: Int!
+    $image: Upload
   ) {
     createProduct(
       data: {
         name: $name
-        status: "DRAFT"
         description: $description
         price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
       }
     ) {
       id
-      name
-      status
-      description
       price
+      description
+      name
     }
   }
 `;
 
 export default function CreateProduct() {
-  // const [name, setName] = useState('Neil');
-
-  const [formGroup, handleChange, resetForm, clearForm] = useForm({
-    name: '',
-    price: '',
-    description: '',
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
     image: '',
-  }); // custom hook in place of useState
-
+    name: 'Nice Shoes',
+    price: 34234,
+    description: 'These are the best shoes!',
+  });
   const [createProduct, { loading, error, data }] = useMutation(
     CREATE_PRODUCT_MUTATION,
     {
-      variables: formGroup,
+      variables: inputs,
     }
   );
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-
-    console.log(formGroup);
-    const result = await createProduct();
-  };
   return (
-    <div>
+    <Form
+      onSubmit={async (e) => {
+        e.preventDefault();
+        console.log(inputs);
+        // Submit the inputfields to the backend:
+        await createProduct();
+        clearForm();
+      }}
+    >
       <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
+        <label htmlFor="image">
+          Image
+          <input
+            required
+            type="file"
+            id="image"
+            name="image"
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="name">
+          Name
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="price">
+          Price
+          <input
+            type="number"
+            id="price"
+            name="price"
+            placeholder="price"
+            value={inputs.price}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <textarea
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={inputs.description}
+            onChange={handleChange}
+          />
+        </label>
 
-      <Form onSubmit={onSubmit}>
-        <fieldset aria-busy={loading} disabled={loading}>
-          <label htmlFor="name">
-            Name
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={formGroup.name}
-              onChange={handleChange}
-            />
-          </label>
-
-          <label htmlFor="price">
-            Price
-            <input
-              type="number"
-              name="price"
-              id="price"
-              value={formGroup.price}
-              onChange={handleChange}
-            />
-          </label>
-
-          <label htmlFor="description">
-            Description
-            <textarea
-              name="description"
-              id="description"
-              value={formGroup.description}
-              onChange={handleChange}
-            />
-          </label>
-
-          <label htmlFor="image">
-            Image
-            <input
-              type="file"
-              name="image"
-              id="image"
-              value={formGroup.image}
-              onChange={handleChange}
-              required
-            />
-          </label>
-
-          <button type="submit">Add Product</button>
-        </fieldset>
-      </Form>
-    </div>
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
   );
 }
