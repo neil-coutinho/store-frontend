2d5a89d07c8566e92bd2a1c2d5c681086236a808
Neil Coutinho <njcoutinho@gmail.com>
2022-02-23 08:02:24 +0530

    single product styles

--- components/SingleProduct.js
+++ components/SingleProduct.js
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client';
 import PropTypes from 'prop-types';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 import DisplayError from './ErrorMessage';
 
 const SINGLE_PRODUCT_QUERY = gql`
@@ -10,11 +11,31 @@ const SINGLE_PRODUCT_QUERY = gql`
       name
       price
       description
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
     }
   }
 `;
+
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
 export default function SingleProduct({ id }) {
-  console.log(id);
   const { loading, error, data } = useQuery(SINGLE_PRODUCT_QUERY, {
     variables: {
       id,
@@ -25,13 +46,20 @@ export default function SingleProduct({ id }) {
 
   if (error) return <DisplayError error={error} />;
   const { Product } = data;
+  const { photo } = Product;
   return (
-    <>
-      <h2>{Product.name}</h2>
+    <ProductStyles>
+      <img
+        loading="lazy"
+        src={photo.image.publicUrlTransformed}
+        alt={photo.altText}
+      />
+
       <div className="details">
-        <div className="description">{Product.description}</div>
+        <h2>{Product.name}</h2>
+        <p className="description">{Product.description}</p>
       </div>
-    </>
+    </ProductStyles>
   );
 }
 
