e50f9a24175a128ad155f26748608071195ffb21
Neil Coutinho <njcoutinho@gmail.com>
2022-03-11 07:56:07 +0530

    pagination _allProductsMeta total count

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,13 +1,35 @@
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 import PaginationStyles from './styles/PaginationStyles';
 
+const ALL_PRODUCTS_META = gql`
+  {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
 export default function Pagination({ page }) {
+  const { error, data, loading } = useQuery(ALL_PRODUCTS_META);
+  console.log(data);
+  if (loading) return null;
+  if (error) return null; // TODO: Show error component
+  const { count } = data._allProductsMeta;
   return (
     <PaginationStyles>
       <Head>
         <title>Sick Fits page {page} of ___</title>
       </Head>
+      <Link href="/">Prev Page</Link>
+
+      <Link href="/">
+        <a>Total items: {count}</a>
+      </Link>
+      <Link href="/">Next Page</Link>
     </PaginationStyles>
   );
 }
